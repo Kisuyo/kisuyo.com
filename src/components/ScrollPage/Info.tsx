55f0d83b2c7ea90604103c3d18af36a61335624e
Kisuyo <ig.intr3st@gmail.com>
2024-01-09 13:17:19 +0100

    random routes

--- src/components/ScrollPage/Info.tsx
+++ src/components/ScrollPage/Info.tsx
@@ -11,7 +11,23 @@ export default function Info() {
           Some information you might want to know about me.
         </div>
       </div>
-      <div class="w-[800px] flex flex-wrap gap-2  relative [&>*]:transition-all [&>*]:text-[14px]  [&>*]:bg-neutral-800 [&>*]:border [&>*]:border-[#f6b092] [&>*]:rounded-[4px] [&>*]:w-fit [&>*]:p-2 [&>*]:px-4 "></div>
+      <div class="w-[800px] flex flex-wrap gap-2  relative [&>*]:transition-all [&>*]:text-[14px]  [&>*]:bg-neutral-800 [&>*]:border [&>*]:border-[#f6b092] [&>*]:rounded-[4px] [&>*]:w-fit [&>*]:p-2 [&>*]:px-4 ">
+        <div>I love playing Games and hanging out with friends</div>
+        <div>
+          My main focus is helping build Learn Anything. An open source website
+          for learning any topic as fast as possible.
+        </div>
+        <div>
+          I have also worked on KusKus, a fast todo app with Ai features
+        </div>
+        <div>I try to stay active on twitter and post my ideas there.</div>
+        <div>You can have a look at my code on my Github.</div>
+        <div>You can support what i do on Github. Thank you</div>
+        <div>
+          Feel free to ask me anything on discord or send me a message on
+          twitter
+        </div>
+      </div>
     </div>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { A, Navigate, useNavigate } from "@solidjs/router";
-import { onMount } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import { useGlobal } from "~/Global/global";
 
 export default function Home() {
+  const [routes, setRoutes] = createSignal(["scroll", "backgroundclip"]);
   const navigate = useNavigate();
   const global = useGlobal();
-  // onMount(() => {
-  //   navigate("/backgroundClip");
-  // });
+  onMount(() => {
+    navigate(routes()[Math.floor(Math.random() * routes().length)]);
+  });
   return <div class="text-white">hi</div>;
 }
